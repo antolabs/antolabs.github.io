b7aa7ffc34210bc4c34cb93cc4fda278cc1c3f03
Geunhyeong Lee <ghLee1129@gmail.com>
2026-01-21 15:43:08 -0500

    Page dot problem solved

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,13 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  // Safelist to preserve classes that might be stripped incorrectly
+  safelist: [
+    // What I Do section styles on About page
+    /^what-i-do-/,
+    "what-i-do-section",
+    "what-i-do-list",
+    "what-i-do-icon",
+    "what-i-do-content",
+  ],
 };
