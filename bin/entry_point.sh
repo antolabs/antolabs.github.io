d4094234cc5d3d9e440f7870d2adcdd7ab5a3a44
Geunhyeong Lee <ghLee1129@gmail.com>
2026-01-23 10:04:10 -0500

    Local host working with Docker

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -21,7 +21,10 @@ manage_gemfile_lock() {
 
 start_jekyll() {
     manage_gemfile_lock
-    bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling &
+    chown -R jekyll:ruby /srv/jekyll
+    su jekyll -c "bundle install" && \
+    su jekyll -c "mkdir -p .jekyll-cache" && \
+    su jekyll -c "bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling" &
 }
 
 start_jekyll
